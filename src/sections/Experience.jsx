d530aaebf2cc5d067cdb509ba896ded5b220de35
Ramaka.Suprasanna <ramakasuprasanna6@gmail.com>
2026-01-26 23:47:42 +0530

    init

--- src/sections/Experience.jsx
+++ src/sections/Experience.jsx
@@ -1,3 +1,21 @@
+const   experiences = [
+    {
+        period:"Oct 2024 -Jan 2025",
+        role: "web devloper",
+        company:"Team-Up",
+        description:"Leading frontend architecture for a suite of finetech products.Implemented",
+        technologies:["Javascript","React","Redux","TailwindCSS","NextJS"],
+        current: false,
+    },
+    {
+        period:"Jul 2025 -Jan 2026",
+        role: " Full-Stack devloper",
+        company:"Labmentrix",
+        description:"Build and maintained full-stack applications and deployed them",
+        technologies:["Javascript","React","Redux","TailwindCSS","NextJS","ExpressJS","MONGODB"],
+        current: false,
+    },
+];
 export const Experience = () =>
 {
     return <section></section>
