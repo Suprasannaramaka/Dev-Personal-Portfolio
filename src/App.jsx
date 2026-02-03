87d32b25ef6b8fd7b1d57c398275eca5fd0176e9
Ramaka.Suprasanna <ramakasuprasanna6@gmail.com>
2026-02-03 23:54:25 +0530

    i

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { Hero } from "./sections/Hero";
 import {About} from "./sections/About";
 import {Experience} from "./sections/Experience";
-import {Testimonials} from "./sections/Testimonials";
 import {Contact} from "./sections/Contact";
 import {Projects} from "./sections/Projects";
 import {Navbar} from "./layout/Navbar";
