0612bfc032855b3620506b15fb1b86a90803e899
Ramaka.Suprasanna <ramakasuprasanna6@gmail.com>
2026-02-16 22:35:52 +0530

    init

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -38,7 +38,7 @@ export const Projects = () =>
   <div className="container mx-auto px-6 relative z-10">
       {/* Section Headers*/}
       <div className="text-center mx-auto max-w-3xl mb-16">
-        <span className="text-secondary-foreground text-sm font-medium tracking mb-10 wider uppercase animate-fade-in">Featured Work</span>
+        <span className="text-secondary-foreground text-sm gap-1.5 font-medium tracking mb-10 wider uppercase animate-fade-in">Featured Work</span>
         <h2 className="text-4xl md:text-5xl font-bold mt-4 mb-6 animate-fade-in animation-delay-100 text-secondary-foreground">projects that 
           <span className="font-serif italic font-normal text-white"> {""} make an impact.</span>
         </h2>
