387d023360747eecc2e9b23c7a42d938d7e396be
Ramaka.Suprasanna <ramakasuprasanna6@gmail.com>
2026-07-02 20:31:00 +0530

    init

--- src/sections/Experience.jsx
+++ src/sections/Experience.jsx
@@ -69,7 +69,7 @@ export const Experience = () => {
               {Experiences.map((exp, idx) => (
                 <div
                   key={idx}
-                  className="relative grid grid-cols-3 md:grid-cols-3 gap-5 animate-fade-in"
+                  className="relative grid grid-cols-3 lg:grid-cols-1 gap-5 animate-fade-in"
                   style={{ animationDelay: `${(idx + 1) * 150}ms` }}
                 >
                   {/*Timeline Dot*/}
@@ -102,15 +102,15 @@ export const Experience = () => {
       <section className="py-10 relative overflow-hidden">
         <div className="absolute top-1/2 left-1/4 w-100 h-100 bg-primary/5 rounded-full blur-3xl -translate-y-1/2" />
         <div className="container mx-auto px-6 relative z-10">
-          <h3 className="text-2xl font-bold mb-10 text-primary/90 text-center md:text-left md:pl-12">Education & Training</h3>
+          <h3 className="text-2xl font-bold mb-10 text-primary/90 text-center md:text-left md:pl-12">Education & Trainings</h3>
           <div className="relative">
             {/* Separate timeline line for courses section */}
             <div className="timeline-glow absolute left-0 md:left-1/2 top-0 bottom-0 w-[0.5px] bg-linear-to-b from-primary/70 via-primary/30 to-transparent md:translate-x-1/2 shadow-[0_0_25px_rgba(32,178,166,0.8)]" />
             <div className="space-y-10 flex flex-col">
               {Courses.map((course, idx) => (
-                <div key={idx} className="relative grid grid-cols-1 md:grid-cols-2 gap-5">
+                <div key={idx} className="relative grid grid-cols-1 lg:grid-cols-2 gap-5">
                   {/* Timeline Dot */}
-                  <div className="absolute left-0 md:left-1/2 top-0 w-3 h-3 bg-primary rounded-full -translate-x-1/2 ring-4 ring-background z-10" />
+                  <div className="absolute left-0 md:left-1/2 top-0  bg-primary rounded-full -translate-x-1/2 ring-4 ring-background z-10" />
                   {/* Alternating Content Sides */}
                   <div className={`pl-8 md:pl-0 w-full ${idx % 2 === 0 ? "md:pr-16 md:text-right md:col-start-1" : "md:col-start-2 md:pl-16"}`}>
                     <div className="glass p-6 rounded-2xl border border-primary/30 hover:border-primary/50 transition-all duration-500">
